e472b1d6c1daa76b3a99a34becca48bbe83f5cad
gluha228 <alexander.glushcov@gmail.com>
2021-12-05 21:33:11 +0200

    Vue-js

--- js.js
+++ js.js
@@ -1,60 +1,5 @@
-document.querySelector('.sidebar').hidden = true;
-function getmenu(){
-    let doc = document.querySelector('.sidebar');
-    if (doc.hidden === true) { doc.hidden = false
-    } else { doc.hidden = true}
-}
-const itemsSourse = [
-    ["Vostok precision class", "Urss", 1965, "Wristwatch", "Wostok 2809", "Wostok", "Swiss", "Manual","0"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
-    ["Roamer Jubile", "Swiss", 1958, "Wristwatch", "MST 402", "Roamer", "Swiss", "Manual","2"],
-    ["Cylindre 4 rubis", "France", 1890, "Pocket watch", "38mm", "Remontoir","Cylinder", "Key","3"],
-    ["Vostok precision class", "Urss", 1962, "Wristwatch", "Wostok 2809A", "Wostok", "Swiss", "Manual","4"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
-    ["Vostok precision class", "Urss", 1965, "Wristwatch", "Wostok 2809", "Wostok", "Swiss", "Manual","0"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
-    ["Roamer Jubile", "Swiss", 1958, "Wristwatch", "MST 402", "Roamer", "Swiss", "Manual","2"],
-    ["Cylindre 4 rubis", "France", 1890, "Pocket watch", "38mm", "Remontoir","Cylinder", "Key","3"],
-    ["Vostok precision class", "Urss", 1962, "Wristwatch", "Wostok 2809A", "Wostok", "Swiss", "Manual","4"],    ["Vostok precision class", "Urss", 1965, "Wristwatch", "Wostok 2809", "Wostok", "Swiss", "Manual","0"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
-    ["Roamer Jubile", "Swiss", 1958, "Wristwatch", "MST 402", "Roamer", "Swiss", "Manual","2"],
-    ["Cylindre 4 rubis", "France", 1890, "Pocket watch", "38mm", "Remontoir","Cylinder", "Key","3"],
-    ["Vostok precision class", "Urss", 1962, "Wristwatch", "Wostok 2809A", "Wostok", "Swiss", "Manual","4"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
-    ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"]];
-const sourseOrder = {'Year' : 2};
+/*
 
-var itemsOrder = [];
-for (var i = 0; i <= itemsSourse.length-1; i++) {
-   itemsOrder.push(i);
-}
-
-function assignItems(){
-    let items = document.querySelector('.items');
-    items.innerHTML = "";
-if (items != null) {
-    for (var itemId = 0;  itemId<itemsSourse.length; itemId++) {
-    items.insertAdjacentHTML("beforeend",
-    '<div class="collection-item"><img src="images/watches/'+itemsSourse[itemsOrder[itemId]][8]+'.jpg" alt="watch"><table><caption><a href="'+itemsSourse[itemsOrder[itemId]][8]+'.html"><h3>'+itemsSourse[itemsOrder[itemId]][0]+
-    '</h3></caption><tbody><tr><td>Manufacturer</td><td>'+itemsSourse[itemsOrder[itemId]][1]+
-    '</td></tr><tr><td>Production year</td><td>'+itemsSourse[itemsOrder[itemId]][2]+
-    '</td></tr><tr><td>Type</td><td>'+itemsSourse[itemsOrder[itemId]][3]+
-    '</td></tr><tr><td>Caliber</td><td>'+itemsSourse[itemsOrder[itemId]][4]+
-    '</td></tr><tr><td>Brand</td><td>'+itemsSourse[itemsOrder[itemId]][5]+
-    '</td></tr><tr><td>Escapement</td><td>' +itemsSourse[itemsOrder[itemId]][6]+
-    '</td></tr><tr><td>Winding</td><td>'+itemsSourse[itemsOrder[itemId]][7]+'</td></tr></tbody></table></div>'
-    );
-}
-}
-}
-assignItems();
 function filter(s){
     let cont = document.getElementById("filter"+s);
     console.log(cont.innerHTML);
@@ -98,6 +43,93 @@ function filter(s){
         break;
     }
 }
-
-
+*/
+const assignItems = new Vue({
+    el: '#items',
+    data: {
+        itemsSourse : [
+            ["Vostok precision class", "Urss", 1965, "Wristwatch", "Wostok 2809", "Wostok", "Swiss", "Manual","0"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
+            ["Roamer Jubile", "Swiss", 1958, "Wristwatch", "MST 402", "Roamer", "Swiss", "Manual","2"],
+            ["Cylindre 4 rubis", "France", 1890, "Pocket watch", "38mm", "Remontoir","Cylinder", "Key","3"],
+            ["Vostok precision class", "Urss", 1962, "Wristwatch", "Wostok 2809A", "Wostok", "Swiss", "Manual","4"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
+            ["Vostok precision class", "Urss", 1965, "Wristwatch", "Wostok 2809", "Wostok", "Swiss", "Manual","0"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
+            ["Roamer Jubile", "Swiss", 1958, "Wristwatch", "MST 402", "Roamer", "Swiss", "Manual","2"],
+            ["Cylindre 4 rubis", "France", 1890, "Pocket watch", "38mm", "Remontoir","Cylinder", "Key","3"],
+            ["Vostok precision class", "Urss", 1962, "Wristwatch", "Wostok 2809A", "Wostok", "Swiss", "Manual","4"],
+            ["Vostok precision class", "Urss", 1965, "Wristwatch", "Wostok 2809", "Wostok", "Swiss", "Manual","0"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
+            ["Roamer Jubile", "Swiss", 1958, "Wristwatch", "MST 402", "Roamer", "Swiss", "Manual","2"],
+            ["Cylindre 4 rubis", "France", 1890, "Pocket watch", "38mm", "Remontoir","Cylinder", "Key","3"],
+            ["Vostok precision class", "Urss", 1962, "Wristwatch", "Wostok 2809A", "Wostok", "Swiss", "Manual","4"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"],
+            ["Volna", "Urss", 1960, "Wristwatch", "Wostok 2809A", "ChChZ", "Swiss", "Manual","1"]
+        ],
+        currentItemsOrder : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,24,24],
+        sourseOrder : {'Year' : 2}    
+        },
+    methods: {
+        filter(s){
+            var itemsOrder = assignItems.currentItemsOrder;
+            let cont = document.getElementById("filter"+s);
+            console.log(cont.innerHTML);
+            switch(cont.innerHTML) {
+                case s:
+                    cont.innerHTML = s+" ↓";
+                    var temp = 0;
+                    for (var x = 0;  x<assignItems.itemsSourse.length; x++) 
+                        for (var y = assignItems.itemsSourse.length-1; y>x; y--){
+                            if (assignItems.itemsSourse[itemsOrder[y]][assignItems.sourseOrder[s]]>assignItems.itemsSourse[itemsOrder[y-1]][assignItems.sourseOrder[s]]){
+                                temp = itemsOrder[y];
+                                itemsOrder[y] = itemsOrder[y-1];
+                                itemsOrder[y-1] = temp;
+                            }
+                        }
+                        console.log(itemsOrder === assignItems.currentItemsOrder);
+                        assignItems.currentItemsOrder=[];
+                        console.log(itemsOrder === assignItems.currentItemsOrder);
+                        assignItems.currentItemsOrder=itemsOrder;
+                        console.log(itemsOrder === assignItems.currentItemsOrder);
+                break;
+                case s+" ↓":
+                    cont.innerHTML = s+" ↑";
+                    var temp = 0;
+                    for (var x = 0;  x<assignItems.itemsSourse.length; x++) 
+                        for (var y = assignItems.itemsSourse.length-1; y>x; y--){
+                            if (assignItems.itemsSourse[itemsOrder[y]][assignItems.sourseOrder[s]]<assignItems.itemsSourse[itemsOrder[y-1]][assignItems.sourseOrder[s]]){
+                                temp = itemsOrder[y];
+                                itemsOrder[y] = itemsOrder[y-1];
+                                itemsOrder[y-1] = temp;
+                            }
+                        }
+                        console.log(itemsOrder);
+                        assignItems.currentItemsOrder=[];
+                        assignItems.currentItemsOrder=itemsOrder;
+        
+                break;
+                case s+" ↑":
+                    cont.innerHTML = s;
+                    assignItems.currentItemsOrder=arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,24,24];
+                break;
+            }
+        }
+        }
+    
+})
+//assignItems.itemsOrder=[0,1];
+document.querySelector('.sidebar').hidden = true;
+function getmenu(){
+    let doc = document.querySelector('.sidebar');
+    if (doc.hidden === true) { doc.hidden = false
+    } else { doc.hidden = true}
+}
 
